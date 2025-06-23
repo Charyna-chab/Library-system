a488984af7d907ad75dca35aa79267158988165f
SOPHY-168 <emsophy80@gmail.com>
2025-06-23 08:47:57 +0700

    make relationship Member and Authors

--- app/Models/Members.php
+++ app/Models/Members.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Members extends Model
 {
     use HasFactory;
+
+    public function Borrowing()
+    {
+        return $this->hasMany(Borrowing::class);
+    }
 }
